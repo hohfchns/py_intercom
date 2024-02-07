e7fbb572d17e6e38ecc46b882b63d984a7d45f2f
Sahar Shulman <sahars@emg-soft.com>
2024-02-07 17:38:16 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,216 @@
+import sys
+import os
+import subprocess
+import shutil
+import speech_recognition as sr
+from enum import Enum
+
+class PromptType(Enum):
+    USER = 0,
+    COMMAND = 1,
+    JUNK = 2
+
+KEYWORDS = \
+{
+    "en_US": ["intercom"],
+    "he_IL": ["אינטרקום"]
+}
+
+COMMANDS = \
+{
+    "en_US": [
+        {
+            "command": "set_language",
+            "params": ["he_IL"],
+            "generic_name": "Hebrew",
+            "triggers": [["set", "language", "to", "hebrew"]]
+        },
+        {
+            "command": "turn_off",
+            "triggers": [
+                ["turn", "off"],
+                ["shut", "yourself" "off"],
+                ["shut", "yourself" "down"],
+            ]
+        }
+    ],
+    "he_IL": [
+        {
+            "command": "set_language",
+            "generic_name": "English",
+            "params": ["en_US"],
+            "triggers": [
+                ["שנה", "שפה", "ל", "אנגלית"],
+                ["חליף", "שפה", "ל", "אנגלית"],
+            ]
+        },
+        {
+            "command": "turn_off",
+            "triggers": [
+                ["תכבה", "את", "עצמך"],
+                ["תסגור", "את", "עצמך"],
+            ]
+        }
+    ]
+}
+
+CURRENT_LANGUAGE = "he_IL"
+
+shut_down_requested: bool = False
+
+class CommandsInterface():
+    @staticmethod
+    def execute_command(method_name: str, method_params: list):
+        command_function = getattr(CommandsInterface, method_name)
+        print(f"Executing command `{method_name}` with params `{method_params}`")
+        return command_function(*method_params)
+
+    @staticmethod
+    def set_language(to: str) -> str:
+        global CURRENT_LANGUAGE
+        generic_name = COMMANDS[CURRENT_LANGUAGE][0]["generic_name"]
+        CURRENT_LANGUAGE = to
+        return f"Setting language to {generic_name}"
+
+    @staticmethod
+    def turn_off() -> str:
+        global shut_down_requested
+        shut_down_requested = True
+        return f"Shutting down"
+
+def convert_voice_to_text(recognizer: sr.Recognizer, audio) -> tuple[str, int]:
+    try:
+        global CURRENT_LANGUAGE
+        text = recognizer.recognize_google(audio, language=CURRENT_LANGUAGE)
+        return (text, 0)
+    except sr.UnknownValueError:
+        return ("", 0)
+    except sr.RequestError as e:
+        return (f"Error; {e}", 1)
+
+def get_voice_data(recognizer: sr.Recognizer) -> object:
+    global _listen_mutex
+    global _listen_mutex_data
+
+    with sr.Microphone() as mic:
+        print("Listening...")
+
+        recognizer.energy_threshold = 100
+        try:
+            data = recognizer.listen(mic, timeout=3, phrase_time_limit=10)
+        except sr.WaitTimeoutError:
+            data = None
+
+        print("Got audio.")
+        return data
+        
+
+def wait_for_voice_prompt(recognizer: sr.Recognizer, trigger_words:list[str]=[]) -> tuple[str, PromptType]:
+    prompt: str = ""
+
+    def find_command(prompt) -> tuple[str, list]:
+        global CURRENT_LANGUAGE
+        for command in COMMANDS[CURRENT_LANGUAGE]:
+            for trigger in command["triggers"]:
+                valid = True
+                for tw in trigger:
+                    if not tw in prompt:
+                        valid = False
+                        continue
+
+                if valid:
+                    params = command["params"] if "params" in command else []
+                    return command["command"], params
+
+        return "", []
+
+    def should_trigger(words, prompt) -> bool:
+        for w in words:
+            if not (w in prompt):
+                return False
+        return True
+
+    while not should_trigger(trigger_words, prompt.lower()):
+        voice = get_voice_data(recognizer)
+        if voice == None:
+            return "", PromptType.JUNK
+
+        prompt, err = convert_voice_to_text(recognizer, voice)
+
+        if err != 0:
+            raise RuntimeError(f"Parsing voice data received error {err}: `{prompt}`")
+
+        cmd, cmd_params = find_command(prompt)
+        if cmd:
+            print(f"Received command `{cmd}`")
+            response = CommandsInterface.execute_command(cmd, cmd_params)
+            if response:
+                return response, PromptType.COMMAND
+            else:
+                return f"Command `{cmd}` executed.", PromptType.COMMAND
+        else:
+            print(f"Received prompt `{prompt}`")
+
+    return prompt, PromptType.USER
+
+def main(argv) -> int:
+    if sys.platform != "linux":
+        print("Only available on Linux OS")
+        return 1
+
+    gpt_cmd = "chatgpt"
+    if shutil.which(gpt_cmd) == None:
+        print(f"ChatGPT command `{gpt_cmd}` not installed?", file=sys.stderr)
+        return 1
+
+    subprocess.check_call(f"{gpt_cmd} --clear-history", shell=True)
+
+    piper_cmd = "piper-tts"
+    if shutil.which(piper_cmd) == None:
+        print(f"ChatGPT command `{piper_cmd}` not installed?", file=sys.stderr)
+        return 1
+
+    play_cmd = "play"
+    if shutil.which(play_cmd) == None:
+        print(f"ChatGPT command `{play_cmd}` not installed?", file=sys.stderr)
+        return 1
+    
+    voices_path = "/usr/share/piper-voices"
+    if not os.path.isdir(voices_path):
+        try:
+            os.mkdir(voices_path)
+        except Exception as e:
+            print(f"Voices path `{voices_path}` does not exist, and could not create it. Please create the path and import your voices there.\nmkdir error: `{e}`", file=sys.stderr)
+            return 1
+
+    voice = "en/en_US/ryan/low/en_US-ryan-low.onnx"
+
+    if not os.path.isfile(voices_path + "/" + voice):
+        print(f"voice `{voice}` not found in `{voices_path}`.")
+        return 1
+
+    recognizer = sr.Recognizer()
+    while not shut_down_requested:
+        prompt, prompt_type = wait_for_voice_prompt(recognizer, KEYWORDS[CURRENT_LANGUAGE])
+
+        if not prompt:
+            continue
+
+        voices_path = "/usr/share/piper-voices"
+        subprocess.Popen(f"play {voices_path}/beep.mp3", shell=True)
+
+        cmd = ""
+        if prompt_type == PromptType.USER:
+            cmd = f"{gpt_cmd} -q \"{prompt}. Talk to me in English.\" | {piper_cmd} --model {voices_path}/{voice} --output_file /tmp/piper_tmp.wav && {play_cmd} /tmp/piper_tmp.wav"
+        else:
+            cmd = f"echo \"{prompt}\" | {piper_cmd} --model {voices_path}/{voice} --output_file /tmp/piper_tmp.wav && {play_cmd} /tmp/piper_tmp.wav"
+
+        subprocess.Popen(cmd, shell=True)
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main(sys.argv))
+
+
